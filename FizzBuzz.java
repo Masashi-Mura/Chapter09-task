37ecfdcaff497f7949a1211b4b44075756bc6cfe
Masashi <k7007674@gmail.com>
2023-09-22 15:16:15 +0900

    Change if #2

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,30 +1,25 @@
 public class FizzBuzz{
     public static void main(String[] args){
 
-        if(args.length==0){
+        if (args.length == 0) {
             System.out.println("数字を入力してください。プログラムを終了します");
             System.exit(1);
         }
 
         int num = Integer.parseInt(args[0]);
 
-        for(int i=1; i<num+1; i++){
-            if(i%3==0 && i%5!=0){       //3で割り切れる時はFizzを出力
-                System.out.print("Fizz");
+        for (int i = 1; i < num + 1; i++) {
+            String str = "";
+            if(i%3 == 0) {         //3で割り切れる
+                str = "Fizz";
             }
-            else if(i%3!=0 && i%5==0){  //5で割り切れる時はBuzzを出力
-                System.out.print("Buzz");
+            if(i%5 == 0) {         //5で割り切れる
+                str = str + "Buzz";
             }
-            else if(i%3==0 && i%5==0){  //3と5で割り切れる時はFizzBuzzを出力
-                System.out.print("FizzBuzz");
+            if(str.isEmpty()) {     //3と5で割り切れない
+                str = Integer.toString(i);
             }
-            else{                       //3と5どちらも割り切れない場合は数字を出力
-                System.out.print(i);  
-            }
-
-            if(i<num){                  //各判定結果の出力後に半角スペースを出力
-                System.out.print(" ");
-            }
+            System.out.print(str + " ");    //結果出力
         }
     }
 }
